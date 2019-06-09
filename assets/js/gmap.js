6a88c539f0b5df10d2361ca077e95012bd1c9cc8
duneoz <nicholas.charles.meyers@gmail.com>
2019-06-08 21:59:48 -0700

    added school, work, and special project arrays, generate markers with custom icons on map

--- assets/js/gmap.js
+++ assets/js/gmap.js
@@ -230,12 +230,94 @@ function initMap() {
             }
         ]
     })
-    var marker = new google.maps.Marker({
-        position: locationRio,
-        map: map.marker,
-        title: 'work please'
-    });
+
+    setWorkMarker(map);
+    setSchoolMarker(map);
+    setSpecprojMarker(map);
+
+};
+
+//array variables for work, school, spec proj
+var work = [
+    ['Fort Worth, TX', 32.7555, -97.3308],
+    ['Amarillo, TX', 35.2220, -101.8313],
+    ['Augusta, GA', 33.4735, -82.0105],
+    ['Memphis, TN', 35.1495, -90.0490],
+    ['Providence, RI', 41.8240, -71.4128],
+    ['Wichita, KS', 37.6872, -97.3301],
+    ['Portland, OR', 45.5155, -122.6793]
+]
+
+var schools = [
+    ['Oregon University', 45.5155, -122.6793],
+    ['Clemson University', 34.6834, -82.8374],
+    ['TCU', 32.7555, -97.3308]
+]
+
+var specprojs = [
+    ['Strategic Planning Retreat | Cabo San Lucas, Baja California Sur, Mexico', 22.8905, -109.9167],
+    ['Safety Audit | Yangjiang, Guangdong, China', 21.8580, 111.9822],
+    ['Energy Audit | Chihuahua City, Chihuahua, Mexico', 28.6330, -106.0691]
+]
+
+function setWorkMarker(map) {
+    var workIcon = {
+        url: '../assets/images/work.png',
+        size: new google.maps.Size(35, 31),
+    };
+
+    for (var i = 0; i < work.length; i++) {
+        var city = work[i];
+        var marker = new google.maps.Marker({
+            position: { lat: city[1], lng: city[2] },
+            map: map,
+            icon: workIcon,
+            title: city[0]
+        });
+    }
+    console.log("this fired");
+};
+
+function setSchoolMarker(map) {
+    var schoolIcon = {
+        url: '../assets/images/school.png',
+        size: new google.maps.Size(31, 31),
+    };
+
+    for (var i = 0; i < schools.length; i++) {
+        var school = schools[i];
+        var marker = new google.maps.Marker({
+            position: { lat: school[1], lng: school[2] },
+            map: map,
+            icon: schoolIcon,
+            title: school[0]
+        });
+    };
+};
+
+function setSpecprojMarker(map) {
+    var specprojIcon = {
+        url: '../assets/images/specproj.png',
+        size: new google.maps.Size(48, 48),
+    };
+
+    for (var i = 0; i < specprojs.length; i++) {
+        var specproj = specprojs[i];
+        var marker = new google.maps.Marker({
+            position: {lat: specproj[1], lng: specproj[2]},
+            map: map,
+            icon: specprojIcon,
+            title: specproj[0]
+        });
+    };
 };
 
 
+
+//     var specprojIcon = {
+//         url: 'assets/images/specproj.png',
+//         size: new google.maps.Size(48, 48),
+//         origin: new google.maps.Point(0, 0),
+//         anchor: new google.maps.Point(0, 48)
+//     };
 
