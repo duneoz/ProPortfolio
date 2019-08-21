b165b95d519039b4ae3beae9becab3a382261c82
duneoz <nicholas.charles.meyers@gmail.com>
2019-08-20 20:56:51 -0700

    auto resize text on resume page is working

--- assets/js/gmap.js
+++ assets/js/gmap.js
@@ -905,7 +905,8 @@ function getTheDetail() {
         if (resElements[i].elementID === checkIt) {
 
             for (j = 0; j < resElements[i].notes.length; j++) {
-                document.getElementById('detail').innerHTML += "<li style='text-align: left; padding: 20px; font-size: 1.25vw;'>" + resElements[i].notes[j] + "</li>";
+                document.getElementById('detail').innerHTML += "<li style='text-align: left; padding: 10px; '>" + resElements[i].notes[j] + "</li>";
+                autoSizeText();
             };
         }
         else {
@@ -986,7 +987,33 @@ varListenForMeToChange.registerListener(function (val) {
     }
 });
 
-
+var autoSizeText;
+autoSizeText = function() {
+  var el, elements, _i, _len, _results;
+  elements = $('.resize');
+  console.log(elements);
+  if (elements.length < 0) {
+    return;
+  }
+  _results = [];
+  for (_i = 0, _len = elements.length; _i < _len; _i++) {
+    el = elements[_i];
+    _results.push((function(el) {
+      var resizeText, _results1;
+      resizeText = function() {
+        var elNewFontSize;
+        elNewFontSize = (parseInt($(el).css('font-size').slice(0, -2)) - 1) + 'px';
+        return $(el).css('font-size', elNewFontSize);
+      };
+      _results1 = [];
+      while (el.scrollHeight > el.offsetHeight) {
+        _results1.push(resizeText());
+      }
+      return _results1;
+    })(el));
+  }
+  return _results;
+};
 
 // function delayZooms() {
 
